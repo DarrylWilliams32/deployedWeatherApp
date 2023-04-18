d8734ffb3b81370086358c48dbdfd5f11b16f2ad
DarrylWilliams32 <77248777+DarrylWilliams32@users.noreply.github.com>
2023-04-18 11:10:44 -0400

    started adding DOM changing code to JS

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
-/*
-<script>
-    let params = new URLSearchParams({
-        access_key: 'b80f7718029ae9b03fc7cc661a0543a7',
-        query: 'New York',
-        units: 'f'
-    });
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
-    fetch(`http://api.weatherstack.com/current?${params}`)
-    .then(res => res.json()).then(console.log);
-</script>
 
-<script src="scripts.js"></script> 
+let params = new URLSearchParams({
+    access_key: 'b80f7718029ae9b03fc7cc661a0543a7',
+    query: 'Cincinnati',
+    units: 'f'
+});
+
+fetch(`http://api.weatherstack.com/current?${params}`)
+.then(res => res.json()).then(console.log);
